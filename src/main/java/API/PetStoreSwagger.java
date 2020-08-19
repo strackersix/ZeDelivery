6b2c2ffe43da1d0a725cc3242b4e87a0575f4e85
Gabriel <strackersix@gmail.com>
2020-08-18 22:38:18 -0300

    Atualização do projeto.

--- src/main/java/API/PetStoreSwagger.java
+++ src/main/java/API/PetStoreSwagger.java
@@ -3,19 +3,19 @@ package API;
 public interface PetStoreSwagger {
 	
 	String T_01_inserirUsuariosJson = "[{\"id\": 1, \"username\": \"ana.maria\", \"firstName\": \"Ana\", \"lastName\": \"Maria\", \"email\": \"ana.maria@itau.com.br\", \"password\": \"123456\", \"phone\": \"11981699678\", \"userStatus\": 1 } , { \"id\": 2, \"username\": \"rodrigo.mendes\", \"firstName\": \"Rodrigo\", \"lastName\": \"Mendes\", \"email\": \"rodrigo.mendes@itau.com.br\", \"password\": \"123456\", \"phone\": \"11981699678\", \"userStatus\": 1 } , { \"id\": 3, \"username\": \"tatiana.vasconcelos\", \"firstName\": \"Tatiana\", \"lastName\": \"Vasconcelos\", \"email\": \"tatiana.vasconcelos@itau.com.br\", \"password\": \"123456\", \"phone\": \"11981699678\", \"userStatus\": 1 }]";
-	String T_01_inserirUsuariosEndpoint = "v2/user/createWithList";
+	String inserirUsuariosEndpoint = "v2/user/createWithList";
 	
 	String T_02_inserirPetsJson = "{ \"id\": 1, \"category\": { \"id\": 10, \"name\": \"Snoopy\" }, \"name\": \"Bichento\", \"photoUrls\": [ \"cat\" ], \"tags\": [ { \"id\": 20, \"name\": \"Perry\" } ], \"status\": \"Ativado\" }";
-	String T_02_inserirPetsEndpoint = "v2/pet";
+	String inserirPets = "v2/pet";
 	
 	String T_03_vendaPetParryAnaMariaJson = "{ \"id\": 1, \"petId\": 20, \"quantity\": 1, \"shipDate\": \"2020-06-21T16:02:27.627Z\", \"status\": \"placed\", \"complete\": true }";
-	String T_03_vendaPetParryAnaMariaEndpoint = "v2/store/order";
+	String vendaPetParryAnaMaria = "v2/store/order";
 	
 	String T_04_vendaPetSnoopyRodrigoMendesJson = "{ \"id\": 2, \"petId\": 10, \"quantity\": 1, \"shipDate\": \"2020-06-21T16:03:27.627Z\", \"status\": \"placed\", \"complete\": true }";
-	String T_04_vendaPetSnoopyRodrigoMendesEndpoint = "v2/store/order";
+	String vendaPetSnoopyRodrigoMendes = "v2/store/order";
 	
 	String T_05_vendaPetSnoopyTatianaVasconcelosJson = "{ \"id\": 3, \"petId\": 10, \"quantity\": 1, \"shipDate\": \"2020-06-21T16:03:27.627Z\", \"status\": \"placed\", \"complete\": true }";
-	String T_05_vendaPetSnoopyTatianaVasconcelosEndpoint = "v2/store/order";
+	String vendaPetSnoopyTatianaVasconcelos = "v2/store/order";
 	
 	String T_06_consultarPrimeiraOrdemJson = "v2/store/order/1";
 	String T_07_consultarSegundaOrdemJson = "v2/store/order/2";
